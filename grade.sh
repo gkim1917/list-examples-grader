9e95cde486a3ec812aaedfd8d2f286dbb5c2261f
gkim1917 <gunju1917@gmail.com>
2022-11-28 06:01:36 -0800

    jn

--- grade.sh
+++ grade.sh
@@ -1,23 +1,41 @@
 # Create your grading script here
-CPATH= ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+
 rm -rf student-submission
 git clone $1 student-submission
+GRADE = "0"
 cd student-submission
 
-if [[ -f "ListExamples.java" ]]
-    then 
-        echo "all the needed files are found"
+if [ -f "ListExamples.java" ]
+then 
+    echo "the file is found"
+else
+    echo "the file is missing"
+    echo "Grade is" $GRADE
+    exit 1
 fi
 
+cd ..
 mkdir testdir
 cp ../TestListExamples.java testdir/
 cp ListExamples.java testdir/
 cd testdir
 javac -cp  $CPATH *.java 2> compile-err.txt
 java -cp $CPATH org.junit.runner.JUnitCore TestListExamples.java
-if [[ $? -eq 0 ]]
+if [ $? -eq 00 ]
 then 
-    echo "it succeeded"
+    echo "compilation succeeded"
+else 
+    echo "compilation failed"
+    echo "Grade is" $GRADE
+    exit 1
+fi
+
+if [ $? -eq 00 ]
+then 
+    GRADE=2
+    echo "Grade is" $GRADE
+else
+    echo "Grade is" $GRADE
 fi
 
 
