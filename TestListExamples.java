e8e006ff153b8fb23420708593101a30843cd44f
gkim1917 <gunju1917@gmail.com>
2022-11-28 05:53:31 -0800

    r

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,34 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestListExamples {
   // Write your grading tests here!
+
+  @Test
+  public void filterTest()
+  {
+    List<String> testlist = new ArrayList<>();
+    testlist.add("apple");
+    testlist.add("grape");
+    testlist.add("watermelon");
+    StringChecker sc = new StringChecker();
+    assertEquals(CorrectListExamples.filter(testlist,sc), ListExamples.filter(testlist,sc));
+  }
+
+  @Test
+  public void mergeTest(){
+    List<String> listone = new ArrayList<>();
+    listone.add("apple");
+    listone.add("grape");
+    listone.add("watermelon");
+    List<String> listtwo = new ArrayList<>();
+    listtwo.add("apple");
+    listtwo.add("grape");
+    listtwo.add("watermelon");
+    assertEquals(CorrectListExamples.merge(listone,listtwo), ListExamples.merge(listone,listtwo));
+
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,23 @@
 # Create your grading script here
-
-set -e
-
+CPATH= ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
+
+if [[ -f "ListExamples.java" ]]
+    then 
+        echo "all the needed files are found"
+fi
+
+mkdir testdir
+cp ../TestListExamples.java testdir/
+cp ListExamples.java testdir/
+cd testdir
+javac -cp  $CPATH *.java 2> compile-err.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples.java
+if [[ $? -eq 0 ]]
+then 
+    echo "it succeeded"
+fi
+
 
